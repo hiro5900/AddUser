353099261a3896c0ce76fe2349c8163be39a15b2
hiro5900 <achyut5900@gmail.com>
2023-05-24 21:05:29 +0530

    Added a delete button to remove users

--- AddUser/script.js
+++ AddUser/script.js
@@ -1,4 +1,4 @@
-document.getElementById('btn').addEventListener('click', ()=>{
+document.getElementById('btn').addEventListener('click', () => {
      let userName = document.getElementsByClassName('user-name')[0].value.trim()
      let userEmail = document.getElementsByClassName('user-email')[0].value
      let userPhone = document.getElementsByClassName('user-phone')[0].value
@@ -12,4 +12,23 @@ document.getElementById('btn').addEventListener('click', ()=>{
      let userInfo = JSON.stringify(info)
 
      localStorage.setItem(userEmail, userInfo)
+
+     let user = document.createElement('li')
+     let deleteBtn = document.createElement('button')
+     deleteBtn.textContent = 'X'
+     deleteBtn.style.borderRadius = '5px'
+     deleteBtn.style.backgroundColor = '#f94a4a'
+     deleteBtn.style.cursor = 'pointer'
+     deleteBtn.className = 'delete-btn'
+
+     user.textContent = userName + (' ') + userEmail + (' ') + userPhone
+     user.appendChild(deleteBtn)
+
+     let users = document.getElementsByClassName('users')[0]
+     users.appendChild(user)
+
+     deleteBtn.addEventListener('click', () => {
+          users.removeChild(user)
+          localStorage.removeItem(userEmail)
+     })
 })
