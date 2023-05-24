b5669930b731ade2dbbbacaa834073c4bd145c8b
hiro5900 <achyut5900@gmail.com>
2023-05-24 21:46:06 +0530

    Added an edit button

--- AddUser/script.js
+++ AddUser/script.js
@@ -1,3 +1,4 @@
+
 document.getElementById('btn').addEventListener('click', () => {
      let userName = document.getElementsByClassName('user-name')[0].value.trim()
      let userEmail = document.getElementsByClassName('user-email')[0].value
@@ -11,9 +12,13 @@ document.getElementById('btn').addEventListener('click', () => {
 
      let userInfo = JSON.stringify(info)
 
+     // store in localStorage
      localStorage.setItem(userEmail, userInfo)
 
+
+     // new list item 
      let user = document.createElement('li')
+     // delete button
      let deleteBtn = document.createElement('button')
      deleteBtn.textContent = 'X'
      deleteBtn.style.borderRadius = '5px'
@@ -21,7 +26,16 @@ document.getElementById('btn').addEventListener('click', () => {
      deleteBtn.style.cursor = 'pointer'
      deleteBtn.className = 'delete-btn'
 
+     // edit button
+     let editBtn = document.createElement('button')
+     editBtn.textContent = 'Edit'
+     editBtn.style.borderRadius = '5px'
+     editBtn.style.cursor = 'pointer'
+     editBtn.className = 'edit-btn'
+
+     // append in users 
      user.textContent = userName + (' ') + userEmail + (' ') + userPhone
+     user.appendChild(editBtn)
      user.appendChild(deleteBtn)
 
      let users = document.getElementsByClassName('users')[0]
@@ -31,4 +45,18 @@ document.getElementById('btn').addEventListener('click', () => {
           users.removeChild(user)
           localStorage.removeItem(userEmail)
      })
+
+     editBtn.addEventListener('click', () => {
+          users.removeChild(user)
+          localStorage.removeItem(userEmail)
+
+          document.getElementsByClassName('user-name')[0].value = info.Name
+          document.getElementsByClassName('user-email')[0].value = info.Email
+          document.getElementsByClassName('user-phone')[0].value = info.Phone
+     })
+
+     // clear input fields after submit
+     document.getElementsByClassName('user-name')[0].value = '';
+     document.getElementsByClassName('user-email')[0].value = '';
+     document.getElementsByClassName('user-phone')[0].value = '';
 })
